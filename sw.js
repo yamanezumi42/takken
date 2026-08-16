27657ccbb6c61d97ec2105a985a2d77322017ead
yamanezumi42 <yamanezumi42@gmail.com>
2026-08-16 12:42:23 +0900

    「1 / 90」の左右でフォントが違っていたのを揃える
    左の数字は桁ロールの部品、右は素のテキストで、器に等幅の指定が無かったため
    左右でフォントが変わり、位置もずれて見えていた。器（.qcnt）にまとめて
    font-family:var(--num) を指定して揃えた。
    ヘッダーの「1 / 1問」で直したのと同じ型が、ここに残っていた。

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 殻だけをキャッシュしてオフラインで起動できるようにする。
    問題データはここに入れない（IndexedDBにある）。 */
-var V='takken-7c593afb';
-var ASSETS=['./','./index.html','./app.js?v=7c593afb','./manifest.webmanifest',
+var V='takken-6cdbdc6a';
+var ASSETS=['./','./index.html','./app.js?v=6cdbdc6a','./manifest.webmanifest',
             './icon-180.png','./icon-192.png','./icon-512.png',
             './fonts/zenoldmincho-subset.woff2','./fonts/washi.png'];
 self.addEventListener('install',function(e){
