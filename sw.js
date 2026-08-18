ed23734433fc60083e0169160516552f821428ed
yamanezumi42 <yamanezumi42@gmail.com>
2026-08-18 19:44:59 +0900

    チェック round17：チェック用問題（1問）

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 殻だけをキャッシュしてオフラインで起動できるようにする。
    問題データはここに入れない（IndexedDBにある）。 */
-var V='takken-79495075';
-var ASSETS=['./','./index.html','./app.js?v=79495075','./bootfx.js?v=79495075','./manifest.webmanifest',
+var V='takken-1785b13f';
+var ASSETS=['./','./index.html','./app.js?v=1785b13f','./bootfx.js?v=1785b13f','./manifest.webmanifest',
             './icon-180.png','./icon-192.png','./icon-512.png',
             './fonts/zenoldmincho-subset.woff2','./fonts/washi.png'];
 self.addEventListener('install',function(e){
