3a7471280e6c2c5bbcfad1ec6c166d8b5cf7ba88
yamanezumi42 <yamanezumi42@gmail.com>
2026-08-18 16:52:00 +0900

    チェック round8：チェック用問題（5問）

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 殻だけをキャッシュしてオフラインで起動できるようにする。
    問題データはここに入れない（IndexedDBにある）。 */
-var V='takken-7d2ee71f';
-var ASSETS=['./','./index.html','./app.js?v=7d2ee71f','./manifest.webmanifest',
+var V='takken-c7eae91a';
+var ASSETS=['./','./index.html','./app.js?v=c7eae91a','./manifest.webmanifest',
             './icon-180.png','./icon-192.png','./icon-512.png',
             './fonts/zenoldmincho-subset.woff2','./fonts/washi.png'];
 self.addEventListener('install',function(e){
