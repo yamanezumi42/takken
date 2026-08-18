9ee0a1b783177a20bbb87f691f28cabc965732df
yamanezumi42 <yamanezumi42@gmail.com>
2026-08-18 17:03:30 +0900

    チェック round9：チェック用問題（10問）

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 殻だけをキャッシュしてオフラインで起動できるようにする。
    問題データはここに入れない（IndexedDBにある）。 */
-var V='takken-c7eae91a';
-var ASSETS=['./','./index.html','./app.js?v=c7eae91a','./manifest.webmanifest',
+var V='takken-2fc0bdd5';
+var ASSETS=['./','./index.html','./app.js?v=2fc0bdd5','./manifest.webmanifest',
             './icon-180.png','./icon-192.png','./icon-512.png',
             './fonts/zenoldmincho-subset.woff2','./fonts/washi.png'];
 self.addEventListener('install',function(e){
