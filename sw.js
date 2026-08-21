82d15a97cffef54b88fcea3b6e9c5573ff01fd41
yamanezumi42 <yamanezumi42@gmail.com>
2026-08-22 03:44:20 +0900

    チェック round66：権利関係 #22 不動産登記法1（27問）

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 殻だけをキャッシュしてオフラインで起動できるようにする。
    問題データはここに入れない（IndexedDBにある）。 */
-var V='takken-2a7aee22';
-var ASSETS=['./','./index.html','./app.js?v=2a7aee22','./bootfx.js?v=2a7aee22','./manifest.webmanifest',
+var V='takken-333bb6ec';
+var ASSETS=['./','./index.html','./app.js?v=333bb6ec','./bootfx.js?v=333bb6ec','./manifest.webmanifest',
             './icon-180.png','./icon-192.png','./icon-512.png',
             './fonts/zenoldmincho-subset.woff2','./fonts/washi.png'];
 self.addEventListener('install',function(e){
