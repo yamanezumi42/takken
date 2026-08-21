9d8e64c29b38082d7cbc337e6ba8c435ae3099b6
yamanezumi42 <yamanezumi42@gmail.com>
2026-08-22 03:43:45 +0900

    チェック round65：権利関係 #20 債権譲渡（28問）

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 殻だけをキャッシュしてオフラインで起動できるようにする。
    問題データはここに入れない（IndexedDBにある）。 */
-var V='takken-9eca856e';
-var ASSETS=['./','./index.html','./app.js?v=9eca856e','./bootfx.js?v=9eca856e','./manifest.webmanifest',
+var V='takken-2a7aee22';
+var ASSETS=['./','./index.html','./app.js?v=2a7aee22','./bootfx.js?v=2a7aee22','./manifest.webmanifest',
             './icon-180.png','./icon-192.png','./icon-512.png',
             './fonts/zenoldmincho-subset.woff2','./fonts/washi.png'];
 self.addEventListener('install',function(e){
