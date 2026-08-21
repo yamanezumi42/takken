fb7621bb662516d0fb24a207e9431fd2a6ba4d51
yamanezumi42 <yamanezumi42@gmail.com>
2026-08-22 01:34:32 +0900

    チェック round32：権利関係 #5 意思表示2（25問）

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 殻だけをキャッシュしてオフラインで起動できるようにする。
    問題データはここに入れない（IndexedDBにある）。 */
-var V='takken-e3e79b38';
-var ASSETS=['./','./index.html','./app.js?v=e3e79b38','./bootfx.js?v=e3e79b38','./manifest.webmanifest',
+var V='takken-6a1ca9f2';
+var ASSETS=['./','./index.html','./app.js?v=6a1ca9f2','./bootfx.js?v=6a1ca9f2','./manifest.webmanifest',
             './icon-180.png','./icon-192.png','./icon-512.png',
             './fonts/zenoldmincho-subset.woff2','./fonts/washi.png'];
 self.addEventListener('install',function(e){
