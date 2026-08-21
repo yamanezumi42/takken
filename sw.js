ba61939c815761b7aa084ea92c1c2213b16c0150
yamanezumi42 <yamanezumi42@gmail.com>
2026-08-22 02:03:27 +0900

    チェック round40：権利関係 #10 債務不履行　損害賠償（24問）

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 殻だけをキャッシュしてオフラインで起動できるようにする。
    問題データはここに入れない（IndexedDBにある）。 */
-var V='takken-da586978';
-var ASSETS=['./','./index.html','./app.js?v=da586978','./bootfx.js?v=da586978','./manifest.webmanifest',
+var V='takken-a330ae97';
+var ASSETS=['./','./index.html','./app.js?v=a330ae97','./bootfx.js?v=a330ae97','./manifest.webmanifest',
             './icon-180.png','./icon-192.png','./icon-512.png',
             './fonts/zenoldmincho-subset.woff2','./fonts/washi.png'];
 self.addEventListener('install',function(e){
