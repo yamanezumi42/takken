d0aa39daeed195d0e9b4a4d5497bfc58b7beb914
yamanezumi42 <yamanezumi42@gmail.com>
2026-08-22 02:21:02 +0900

    チェック round45：権利関係 #11 契約の解除（32問）

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 殻だけをキャッシュしてオフラインで起動できるようにする。
    問題データはここに入れない（IndexedDBにある）。 */
-var V='takken-dae982dc';
-var ASSETS=['./','./index.html','./app.js?v=dae982dc','./bootfx.js?v=dae982dc','./manifest.webmanifest',
+var V='takken-59fbff4f';
+var ASSETS=['./','./index.html','./app.js?v=59fbff4f','./bootfx.js?v=59fbff4f','./manifest.webmanifest',
             './icon-180.png','./icon-192.png','./icon-512.png',
             './fonts/zenoldmincho-subset.woff2','./fonts/washi.png'];
 self.addEventListener('install',function(e){
