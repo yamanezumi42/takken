07a6a40e97c0f86496462eb59eb1008641690211
yamanezumi42 <yamanezumi42@gmail.com>
2026-08-22 02:49:08 +0900

    チェック round55：権利関係 #15 抵当権2（18問）

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 殻だけをキャッシュしてオフラインで起動できるようにする。
    問題データはここに入れない（IndexedDBにある）。 */
-var V='takken-0980c7a4';
-var ASSETS=['./','./index.html','./app.js?v=0980c7a4','./bootfx.js?v=0980c7a4','./manifest.webmanifest',
+var V='takken-84e3a293';
+var ASSETS=['./','./index.html','./app.js?v=84e3a293','./bootfx.js?v=84e3a293','./manifest.webmanifest',
             './icon-180.png','./icon-192.png','./icon-512.png',
             './fonts/zenoldmincho-subset.woff2','./fonts/washi.png'];
 self.addEventListener('install',function(e){
