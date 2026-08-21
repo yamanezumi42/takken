906d7c3bb141ed9d5ef00ef790adf60923c7146c
yamanezumi42 <yamanezumi42@gmail.com>
2026-08-22 02:22:45 +0900

    チェック round48：権利関係 #12 契約不適合責任（1問）

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 殻だけをキャッシュしてオフラインで起動できるようにする。
    問題データはここに入れない（IndexedDBにある）。 */
-var V='takken-246e192d';
-var ASSETS=['./','./index.html','./app.js?v=246e192d','./bootfx.js?v=246e192d','./manifest.webmanifest',
+var V='takken-64c2b2a4';
+var ASSETS=['./','./index.html','./app.js?v=64c2b2a4','./bootfx.js?v=64c2b2a4','./manifest.webmanifest',
             './icon-180.png','./icon-192.png','./icon-512.png',
             './fonts/zenoldmincho-subset.woff2','./fonts/washi.png'];
 self.addEventListener('install',function(e){
