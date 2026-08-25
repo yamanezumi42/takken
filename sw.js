b179ab6c10456a2c9a3fd1684712731706b209ed
yamanezumi42 <yamanezumi42@gmail.com>
2026-08-25 20:27:01 +0900

    配信 3803892a684dede2

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 殻だけをキャッシュしてオフラインで起動できるようにする。
    問題データはここに入れない（IndexedDBにある）。 */
-var V='takken-352a71b1';
-var ASSETS=['./','./index.html','./app.js?v=352a71b1','./bootfx.js?v=352a71b1','./manifest.webmanifest',
+var V='takken-c10a1271';
+var ASSETS=['./','./index.html','./app.js?v=c10a1271','./bootfx.js?v=c10a1271','./manifest.webmanifest',
             './icon-180.png','./icon-192.png','./icon-512.png',
             './fonts/zenoldmincho-subset.woff2','./fonts/washi.png',
             './lesson/L1.html','./lesson/L2.html','./lesson/L3a.html','./lesson/L3b.html','./lesson/L3c.html','./lesson/L3d.html'];
